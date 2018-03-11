a8dd6add722a1c8921b7e6eaef010572be3e29f0
Andrew Klotz <agc.klotz@gmail.com>
2018-03-11 13:50:11 -0400

    visitor

---
+++ src/behavioral/visitor.cpp
@@ -0,0 +1,61 @@
+class Element {
+  public:
+    virtual int accept(class Visitor *visitor) = 0;
+
+    int getValue() {
+      return value;
+    }
+
+  protected:
+    int value;
+};
+
+class Visitor {
+  public:
+    virtual int visitElementA(Element *element) = 0;
+    virtual int visitElementB(Element *element) = 0;
+};
+
+class ConcreteVisitorA : public Visitor {
+  public:
+    int visitElementA(Element *element) override {
+      return element->getValue() * 10;
+    }
+
+    int visitElementB(Element *element) override {
+      return element->getValue() * 10;
+    }
+};
+
+class ConcreteVisitorB : public Visitor {
+  public:
+    int visitElementA(Element *element) override {
+      return element->getValue() * 20;
+    }
+
+    int visitElementB(Element *element) override {
+      return element->getValue() * 20;
+    }
+};
+
+class ConcreteElementA : public Element {
+  public:
+    ConcreteElementA() {
+      value = 10;
+    }
+
+    int accept(Visitor *visitor) override {
+      return visitor->visitElementA(this);
+    }
+};
+
+class ConcreteElementB : public Element {
+  public:
+    ConcreteElementB() {
+      value = 20;
+    }
+
+    int accept(Visitor *visitor) override {
+      return visitor->visitElementA(this);
+    }
+};
---
+++ test/behavioral/visitor_test.cpp
@@ -0,0 +1,17 @@
+#include <cassert>
+
+#include "../../src/behavioral/visitor.cpp"
+
+int main() {
+  ConcreteElementA *ea = new ConcreteElementA;
+  ConcreteElementB *eb = new ConcreteElementB;
+
+  ConcreteVisitorA *va = new ConcreteVisitorA;
+  ConcreteVisitorB *vb = new ConcreteVisitorB;
+
+  assert(ea->accept(va) == 100);
+  assert(eb->accept(va) == 200);
+
+  assert(ea->accept(vb) == 200);
+  assert(eb->accept(vb) == 400);
+}
