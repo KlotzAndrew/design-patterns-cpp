783a48c73707bc54648146915f65407823ae7850
Andrew Klotz <agc.klotz@gmail.com>
2018-03-11 10:44:17 -0400

    observer

---
+++ test/behavioral/observer_test.cpp
@@ -0,0 +1,27 @@
+#include <cassert>
+
+#include "../../src/behavioral/observer.cpp"
+
+int main() {
+  ConcreteObserver *o1 = new ConcreteObserver;
+  ConcreteObserver *o2 = new ConcreteObserver;
+
+  Subject *subject = new ConcreteSubject;
+
+  subject->attach(o1);
+  subject->attach(o2);
+
+  subject->setState(10);
+  subject->notfiy();
+
+  assert(o1->getState() == 10);
+  assert(o2->getState() == 10);
+
+  subject->detach(0);
+
+  subject->setState(20);
+  subject->notfiy();
+
+  assert(o1->getState() == 10);
+  assert(o2->getState() == 20);
+}
